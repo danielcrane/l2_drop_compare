d209de9a33f6d8463925764c5a49ac24a7b09a73
danielcrane <daniellewiscrane@gmail.com>
2019-08-26 14:33:23 +0900

    Improve output message formatting

--- l2portal_compare.py
+++ l2portal_compare.py
@@ -68,11 +68,10 @@ def compare_to_l2portal(drop_data):
                 if i == 0:
                     print("---Spoils---")
                 print(diff)
-            print("\n")
 
         count += 1
         if count % 25 == 0 or count == num_mobs:
-            print(f"Checked {count} / {num_mobs}")
+            print(f"\nChecked {count} / {num_mobs}\n")
     print("Checking complete")
 
 
