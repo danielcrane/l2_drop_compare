f4da79aa74ea0f26868e9c6d317a5b22e933335a
danielcrane <daniellewiscrane@gmail.com>
2019-08-26 10:45:18 +0900

    Make output messages nicer

--- l2portal_compare.py
+++ l2portal_compare.py
@@ -8,97 +8,136 @@ def compare_to_l2portal(drop_data):
     num_mobs = len(drop_data)
     count = 0
 
-    for key, drop_actual in drop_data.items():
+    for mob_id, drop_actual in drop_data.items():
 
-        url = f'http://gracia.l2portal.com/Npc.aspx?ID={key}'
+        url = f"http://gracia.l2portal.com/Npc.aspx?ID={mob_id}"
 
-        r  = requests.get(url)
+        r = requests.get(url)
         data = r.text
-        soup = BeautifulSoup(data, features='html.parser')
+        soup = BeautifulSoup(data, features="html.parser")
 
-        drop_data = soup.find('table', {'id': 'ctl00_ContentPlaceHolder1_GVNpcDrop'})
-        spoil_data = soup.find('table', {'id': 'ctl00_ContentPlaceHolder1_GVNpcSpoil'})
-
-        for drop_type in ['Drop', 'Spoil']:
-            drops = []
-            data = soup.find('table', {'id': f'ctl00_ContentPlaceHolder1_GVNpc{drop_type}'})
-            for drop in data.find_all('tr'):
-                if drop.has_attr('class') and drop['class'][0] == 'HeaderStyle2':
+        drops = {"drop": [], "spoil": []}
+        for drop_type in drops.keys():
+            data = soup.find(
+                "table", {"id": f"ctl00_ContentPlaceHolder1_GVNpc{drop_type.title()}"}
+            )
+            for drop in data.find_all("tr"):
+                if drop.has_attr("class") and drop["class"][0] == "HeaderStyle2":
                     continue
 
-                parts = drop.find_all('td')
+                parts = drop.find_all("td")
 
                 if len(parts) != 5:
                     # Assume that if len(parts)!=5 then not an item drop and move on:
                     continue
 
-                item_id = eval(parts[1].select('a')[0]['href'].split('=')[-1])
+                item_id = eval(parts[1].select("a")[0]["href"].split("=")[-1])
 
-                for span in parts[3].select('span'):
-                    if 'LblMin' in span['id']:
+                for span in parts[3].select("span"):
+                    if "LblMin" in span["id"]:
                         item_min = eval(span.text)
-                    elif 'LblMax' in span['id']:
+                    elif "LblMax" in span["id"]:
                         item_max = span.text
 
-                if item_max == '':
+                if item_max == "":
                     # If item_max is undefined, set it as same as item_min
                     item_max = item_min
                 else:
                     # Else format accordingly:
-                    item_max = eval(item_max.split('- ')[-1])
+                    item_max = eval(item_max.split("- ")[-1])
 
-                chance = parts[4].select('span')[0].text
+                chance = parts[4].select("span")[0].text
                 # Some of the chances are fractions, and some percent, account for that here:
-                if chance[-1] == '%':
-                    item_chance = eval(chance[:-1])/100
+                if chance[-1] == "%":
+                    item_chance = eval(chance[:-1]) / 100
                 else:
-                    item_chance = eval(parts[4].select('span')[0].text)
+                    item_chance = eval(parts[4].select("span")[0].text)
 
-                drops.append([item_id, item_min, item_max, item_chance])
+                drops[drop_type].append([item_id, item_min, item_max, item_chance])
 
-            same = check_same(drop_actual[drop_type.lower()], drops)
-            if not same:
-                print(f'Check Failed - mob_id: {key} - drop_type: {drop_type.lower()}')
-                print(f'Drops Actual: {drop_actual[drop_type.lower()]})')
-                print(f'Drops Web: {drops}')
+        diff_drop = find_diff(drop_actual["drop"], drops["drop"], type="drop")
+        diff_spoil = find_diff(drop_actual["spoil"], drops["spoil"], type="spoil")
+        if not (diff_drop == [] and diff_spoil == []):
+            print(f"\nCheck Failed - mob_id: {mob_id}")
+            for i, diff in enumerate(diff_drop):
+                if i == 0:
+                    print("---Drops---")
+                print(diff)
+
+            for i, diff in enumerate(diff_spoil):
+                if i == 0:
+                    print("---Spoils---")
+                print(diff)
 
         count += 1
         if count % 25 == 0 or count == num_mobs:
-            print(f'Checked {count} / {num_mobs}')
-    print('Checking complete')
+            print(f"Checked {count} / {num_mobs}")
+    print("Checking complete")
 
-def check_same(drops1, drops2):
+
+def find_diff(drops1, drops2, type):
     # Check if the provided lists of drops are equivalent
+    out = []
     drops1 = np.array(drops1)
     drops2 = np.array(drops2)
 
-    if drops1.shape[0] == 0 and drops2.shape[0] == 0:
-        if drops1.shape[0] != 0 or drops2.shape[0] != 0:
-            return False
-        return True
+    if drops1.shape[0] == 0 or drops2.shape[0] == 0:
+        if drops1.shape[0] != 0:
+            for drop in drops1:
+                out.append(f"Additional {type} in DB: {drop[0]}")
+
+        if drops2.shape[0] != 0:
+            for drop in drops2:
+                out.append(f"Additional {type} in Web: {drop[0]}")
+
+        return out
 
     i = np.argsort(drops1[:, 0])
     drops1 = drops1[i, :]
 
-    i = np.argsort(drops2[:,0])
+    i = np.argsort(drops2[:, 0])
     drops2 = drops2[i, :]
 
     # First check that the drops and amounts are the same:
     if not np.array_equal(drops1[:, :3], drops2[:, :3]):
-        return False
+        if not np.array_equal(drops1[:, 0], drops2[:, 0]):
+            for drop in drops1[:, 0]:
+                if drop not in drops2[:, 0]:
+                    out.append(f"Additional {type} in DB: {drop[0]}")
+
+            for drop in drops2[:, 0]:
+                if drop not in drops1[:, 0]:
+                    out.append(f"Additional {type} in Web: {drop[0]}")
+
+        # Note: Doing in this way may miss cases, should be done differently to be thorough
+        elif not np.array_equal(drops1[:, 1:3], drops2[:, 1:3]):
+            idxs = np.where(drops1[:, 1:3] != drops2[:, 1:3])[0]
+            for idx in idxs:
+                out.add(
+                    f"Unequal {type} amount for item {drops1[idx, 0]}: "
+                    f"{tuple(drops1[idx,1:3])} (DB) vs {tuple(drops2[idx,1:3])} (Web)"
+                )
 
     # Now check that chances are same (within 1% tolerance):
-    if not np.allclose(drops1[:, 3], drops2[:, 3], rtol=0.01):
-        return False
+    rtol = 0.01
+    if not np.allclose(drops1[:, 3], drops2[:, 3], rtol=rtol):
+        diff = np.abs(drops1[:, 3] - drops2[:, 3]) / np.abs(drops2[:, 3])
+        idxs = np.where(diff > rtol)[0]
+        for idx in idxs:
+            out.append(
+                f"Unequal {type} chance for item {drops1[idx, 0]}: "
+                f"{drops1[idx,3]} (DB) vs {drops2[idx,3]} (Web)"
+            )
 
     # If both above checks pass, return true:
-    return True
+    return out
+
 
 def read_drop_file(drop_file):
 
     drop_data = {}
 
-    with open(drop_file, 'r') as f:
+    with open(drop_file, "r") as f:
         line = f.readline().strip()
         empty_count = 0
         while empty_count < 100:
@@ -109,30 +148,30 @@ def read_drop_file(drop_file):
             else:
                 empty_count = 0
 
-            if line[0] == '(' and ('),' in line or ');' in line):
+            if line[0] == "(" and (")," in line or ");" in line):
                 try:
-                    idx = line.index('),')
-                except:
-                    idx = line.index(');')
+                    idx = line.index("),")
+                except ValueError:
+                    idx = line.index(");")
 
-                data = eval(line[:idx+1])
+                data = eval(line[: idx + 1])
 
                 mob_id, item_id, item_min, item_max, category, chance = data
 
                 if mob_id not in drop_data:
-                    drop_data[mob_id] = {'drop': [], 'spoil': []}
+                    drop_data[mob_id] = {"drop": [], "spoil": []}
 
                 if category == -1:
-                    drop_data[mob_id]['spoil'].append([item_id, item_min, item_max, chance/1e6])
+                    drop_data[mob_id]["spoil"].append([item_id, item_min, item_max, chance / 1e6])
                 else:
-                    drop_data[mob_id]['drop'].append([item_id, item_min, item_max, chance/1e6])
+                    drop_data[mob_id]["drop"].append([item_id, item_min, item_max, chance / 1e6])
 
             line = f.readline().strip()
 
     return drop_data
 
 
-if __name__ == '__main__':
-    drop_file = 'droplist.sql'
+if __name__ == "__main__":
+    drop_file = "droplist.sql"
     drop_data_actual = read_drop_file(drop_file)
     compare_to_l2portal(drop_data_actual)
